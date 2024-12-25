8a5d4c6645d3af682ee6de22740db83754e9b476
invia <inviakonstantin@gmail.com>
2024-12-25 03:39:51 +0200

    Added demo cases for the usage

--- Task1.js
+++ Task1.js
@@ -20,3 +20,41 @@ function asyncFind(array, asyncPredicate, callback) {
     };
     next();
 }
+function isEvenAsyn(value, callback) {
+    setTimeout(() => {
+        callback(null, value % 2 === 0);
+    }, 1500);
+}
+
+const data1 = [1, 1, 1, 1, 6];
+console.time("Test 1 time");
+asyncFind(data1, isEvenAsyn, (err, result) => {
+    console.timeEnd("Test 1 time")
+    if (err) {
+        console.error("Помилка:", err);
+    } else {
+        console.log("Знайдений елемент:", result);
+    }
+});
+
+const data2 = [-1, 5, 7, 4, 1];
+console.time("Test 2 time");
+asyncFind(data2, isEvenAsyn, (err, result) => {
+    console.timeEnd("Test 2 time")
+    if (err) {
+        console.error("Помилка:", err);
+    } else {
+        console.log("Знайдений елемент:", result);
+    }
+});
+
+const data3 = [1, 1, 1, 64, 90];
+console.time("Test 3 time");
+asyncFind(data3, isEvenAsyn, (err, result) => {
+    console.timeEnd("Test 3 time")
+    if (err) {
+        console.error("Помилка:", err);
+    } else {
+        console.log("Знайдений елемент:", result);
+    }
+});
