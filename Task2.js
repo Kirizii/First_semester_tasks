0bf6fe4fbb0ac432effaf23aa1eb11e8bd5730a3
invia <inviakonstantin@gmail.com>
2024-12-27 04:40:29 +0200

    Added support for parallelism to Task2

--- Task2.js
+++ Task2.js
@@ -1,25 +1,33 @@
 "use strict"
 
-function asyncFind(array, asyncPredicate) {
+function asyncFind(array, asyncPredicate, parallelism = 2) {
     return new Promise((resolve, reject) => {
-        let index = 0;
+        let completed = 0;
+        let found = false;
+        const promises = new Set();
         const next = () => {
-            if (index >= array.length) {
-                resolve(undefined);
+            if (found || completed >= array.length) {
                 return;
             }
-            asyncPredicate(array[index])
-                .then((result) => {
-                    if (result) {
-                        resolve(array[index]);
-                    } else {
-                        index++;
+            if (promises.size < parallelism) {
+                const index = completed++;
+                const promise = asyncPredicate(array[index])
+                    .then((result) => {
+                        if (result && !found) {
+                            found = true;
+                            resolve(array[index]);
+                        }
+                    })
+                    .catch(reject)
+                    .finally(() => {
+                        promises.delete(promise);
                         next();
-                    }
-                })
-                .catch((err) => {
-                    reject(err);
-                });
+                    });
+                promises.add(promise);
+                next();
+            } else if (promises.size === 0 && completed >= array.length) {
+                resolve(undefined);
+            }
         };
         next();
     });
@@ -32,10 +40,10 @@ function isEvenAsyn(value) {
     }, 1500);})
 }
 
-async function awaitTest(testNumber ,data) {
+async function awaitTest(testNumber ,data, parallelism) {
     try {
         console.time(`Await test ${testNumber} time`)
-        const result = await asyncFind(data, isEvenAsyn);
+        const result = await asyncFind(data, isEvenAsyn,parallelism);
         console.timeEnd(`Await test ${testNumber} time`)
         console.log("Знайдений елемент(async-await):", result);
     } catch (err) {
@@ -43,9 +51,9 @@ async function awaitTest(testNumber ,data) {
     }
 }
 
-function test(testNumber ,data){
+function test(testNumber ,data, parallelism){
     console.time(`Test ${testNumber} time`);
-    asyncFind(data, isEvenAsyn).then((result) => {
+    asyncFind(data, isEvenAsyn, parallelism).then((result) => {
         console.timeEnd(`Test ${testNumber} time`)
         console.log("Знайдений елемент:", result);
     }).catch((err) => {
@@ -56,10 +64,10 @@ const data1 = [1, 1, 1, 1, 6];
 const data2 = [-1, 5, 7, 4, 1];
 const data3 = [1, 1, 1, 64, 90];
 
-test(1, data1)
-test(2, data2)
-test(3, data3)
+test(1, data1, 2)
+test(2, data2, 2)
+test(3, data3, 4)
 
-awaitTest(1, data1);
-awaitTest(2, data2);
-awaitTest(3, data3)
+awaitTest(1, data1, 2);
+awaitTest(2, data2, 2);
+awaitTest(3, data3, 2)
