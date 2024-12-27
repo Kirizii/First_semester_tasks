32c85aa1fb753f9a2c35abe9162cbaa4abb3bd78
invia <inviakonstantin@gmail.com>
2024-12-27 03:50:17 +0200

    EventEmitter chat task finished

---
+++ Task5.js
@@ -0,0 +1,27 @@
+const EventEmitter = require('events');
+
+class User {
+    constructor(name, eventEmitter) {
+        this.name = name;
+        this.eventEmitter = eventEmitter;
+        this.eventEmitter.on('message', ({ user, message }) => {
+            if (user !== this.name) {
+                console.log(`[${this.name}] Отримав повідомлення від [${user}]: ${message}`);
+            }
+        });
+    }
+
+    sendMessage(message) {
+        this.eventEmitter.emit('message', { user: this.name, message });
+    }
+}
+
+const chatEventEmitter = new EventEmitter();
+
+const kostya = new User('Kostya', chatEventEmitter);
+const maks = new User('Maks', chatEventEmitter);
+const sasha = new User('Shasha', chatEventEmitter);
+
+kostya.sendMessage('Привіт усім');
+maks.sendMessage('Привіт');
+sasha.sendMessage('Доброго дня всім');
