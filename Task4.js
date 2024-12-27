b05665f48ac68ec4c0ba4e8ae2c1a3863a855fc5
invia <inviakonstantin@gmail.com>
2024-12-27 02:45:26 +0200

    Added asyncIteratorWrite

--- Task4.js
+++ Task4.js
@@ -7,3 +7,32 @@ async function* randomNumberGenerator() {
         await new Promise((resolve) => setTimeout(resolve, 500));
     }
 }
+
+async function asyncIteratorWrite(filePath, generator, duration) {
+    const writableStream = fs.createWriteStream(filePath);
+    const startTime = Date.now();
+    try {
+        for await (const number of generator) {
+            writableStream.write(`${number}\n`);
+            if (Date.now() - startTime >= duration) {
+                break;
+            }
+        }
+    } catch (err) {
+        console.error("Помилка під час запису:", err.message);
+    } finally {
+        writableStream.end();
+    }
+
+    writableStream.on("finish", () => {
+        console.log("Запис завершено.");
+    });
+
+    writableStream.on("error", (err) => {
+        console.error("Помилка запису:", err.message);
+    });
+}
+
+(async () => {
+    await asyncIteratorWrite("output.txt", randomNumberGenerator(), 6000);
+})();
